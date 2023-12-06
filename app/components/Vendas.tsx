0ddda135b69b266b4f47cf14fce7e82a91a2249e
aaaaalec <aleclinehan@gmail.com>
2023-12-06 01:01:51 +0000

    shadn added

--- app/components/Vendas.tsx
+++ app/components/Vendas.tsx
@@ -1,6 +1,17 @@
-'use client';
+"use client";
 
-import React from 'react'
+import React, {useState} from "react";
+import ShippingInfoModal from "./ShippingInfoModal";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
+import { Button } from "./ui/button";
 
 type Venda = {
   id: string;
@@ -29,54 +40,94 @@ type Props = {
 };
 
 const Vendas = (props: Props) => {
-
   function formatDate(datetime: string) {
     let date = new Date(datetime);
-    let formattedDate = date.getFullYear() + '-' + 
-                       ('0' + (date.getMonth()+1)).slice(-2) + '-' + 
-                       ('0' + date.getDate()).slice(-2);
+    let formattedDate =
+      date.getFullYear() +
+      "-" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + date.getDate()).slice(-2);
     return formattedDate;
   }
 
+  const hasPaidStatus = props.vendas.some(venda => venda.paymentStatus === 'Paid');
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+ 
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <div className='px-2 md:px-6  w-full'>
-        <h1 className='border-b-2 border-white w-full'>Vendas</h1>
-        <table className='overflow-auto table-auto'>
-      <thead>
-        <tr className='bg-red border-2 border-b-white '>
-        <th className='w-12 bg-white/20'>Shipped</th>
-          <th>Amount</th>
-          <th className='min-w-46 bg-white/20'>Description</th>
-          <th>Datetime</th>
-          <th>Payment Status</th>
-          <th>Payment Datetime</th>
-          <th>Shipping Method</th>
-          <th>Shipping Info</th>
-          <th>ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.vendas.map((venda, index) => (
-          <tr key={index}>
-             <td className=' bg-red-200/20'>
-              <input type="radio" id={`shipped-${index}`} name={`shipped-${index}`} />
-              <label htmlFor={`shipped-${index}`}></label>
-            </td>
-            <td className=' bg-red-200/20'>{venda.amount}€</td>
-            <td className=' bg-red-200/20'>{venda.description}</td>
-            <td className=' bg-red-200/20'>{formatDate(venda.datetime)}</td>
-            <td className=' bg-red-200/20'>{venda.paymentStatus}</td>
-            <td className=' bg-red-200/20'>{venda.paymentDatetime}</td>
-            <td className=' bg-red-200/20'>{venda.shippingMethod}</td>
-            <td className=' bg-red-200/20'>{JSON.stringify(venda.shippingInfo)}</td>
-            <td className=' bg-red-200/20'>{venda.id}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="px-2 md:px-6  w-full">
+      <h1 className="border-b-2 border-white w-full">Vendas</h1>
+      <Table className="overflow-auto table-auto">
+        <TableHeader>
+          <TableRow className="bg-red border-2 border-b-white ">
+          <TableHead>Payment Status</TableHead>
+         
+              {hasPaidStatus ? (
+               <>
+           
+            <TableHead>Payment Datetime</TableHead>
+            <TableHead className="w-12 bg-white/20">Shipped</TableHead>
+            </>
+             ) : null}
+            <TableHead>Amount</TableHead>
+            <TableHead className="min-w-46 bg-white/20">Description</TableHead>
+            <TableHead>Datetime</TableHead>
+            <TableHead>Shipping Method</TableHead>
+            <TableHead>Shipping Info</TableHead>
+            <TableHead>ID</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {props.vendas.map((venda, index) => (
+            <>
+            
+            <TableRow className={hasPaidStatus ? "" : "bg-red-500" }key={index}>
+               <TableCell className=" bg-red-200/20">{venda.paymentStatus}</TableCell>
+              {venda.paymentStatus === 'Paid' ? (
+               <>
+                <TableCell className=" bg-red-200/20">{formatDate(venda.paymentDatetime)}</TableCell>
+              <TableCell className=" bg-red-200/20">
+                <input
+                  type="radio"
+                  id={`shipped-${index}`}
+                  name={`shipped-${index}`}
+                />
+                <label htmlFor={`shipped-${index}`}></label>
+              </TableCell>
+               </>
+             ) : null}
+              <TableCell className=" bg-red-200/20">{venda.amount}€</TableCell>
+              <TableCell className=" bg-red-200/20">{venda.description}</TableCell>
+              <TableCell className=" bg-red-200/20">{formatDate(venda.datetime)}</TableCell>
+             
+              <TableCell className=" bg-red-200/20">{venda.shippingMethod}</TableCell>
+              <TableCell className=" bg-red-200/20">
+              <Button className="hover:bg-red-500 bg-slate-500" onClick={handleModalOpen}>Show Shipping Info</Button>
+               <ShippingInfoModal
+        
+            shippingInfo={venda.shippingInfo}
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+          />
+              </TableCell>
+              <TableCell className=" bg-red-200/20">{venda.id}</TableCell>
+            </TableRow>
+           
+         </> ))}
+        </TableBody>
+      </Table>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Vendas
+export default Vendas;
