3275f119e41dd7ade158991f9422899ea639b937
Alec <aleclinehan@gmail.com>
2023-12-18 16:06:53 +0000

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/bike.png.png"
+              alt="Bicycle Logo"
               className="dark:invert"
               width={100}
               height={24}
