d5c794378576feaf3eeaf154e0591e1f3fff8aa4
aaaaalec <aleclinehan@gmail.com>
2024-02-09 14:09:36 -0600

    update

--- app/painel-das-vendas/page.tsx
+++ app/painel-das-vendas/page.tsx
@@ -1,6 +1,6 @@
 import PainelDasVendas from "../components/PainelDasVendas";
-import { groq } from "next-sanity"
-import { client } from "@/sanity/lib/client"
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
 
 export default async function Page() {
   const vendas = await client.fetch(
@@ -26,15 +26,13 @@ export default async function Page() {
       }
     }`,
     {
-      next: { revalidate: 1 },
-    }
-  )
-  
+      cache: "no-store",
+    },
+  );
+
   return (
     <div>
-     <PainelDasVendas vendas={vendas}/>
+      <PainelDasVendas vendas={vendas} />
     </div>
   );
-};
-
-
+}
