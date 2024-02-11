3772fdf8b6dbfd4855825cd1e3e3c728cf239d6c
aaaaalec <aleclinehan@gmail.com>
2024-02-10 21:37:38 -0600

    live

--- app/components/PainelDasVendas.tsx
+++ app/components/PainelDasVendas.tsx
@@ -1,37 +1,46 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { FormEvent, useState, useEffect } from "react";
 import { serialize } from "cookie";
-import { parseCookies } from 'nookies';
-import Vendas from './Vendas';
-
+import { parseCookies } from "nookies";
+import Vendas from "./Vendas";
+import { client } from "../../sanity/lib/client";
 type Props = {
-    vendas: any;
-    };
+  vendas: any;
+};
 
-const PainelDasVendas = ({vendas}: Props) => {
-  const router = useRouter();
+const PainelDasVendas = ({ vendas }: Props) => {
   const [password, setPassword] = useState(false);
-  const [filteredVendas, setFilteredVendas] = useState([]);
+  const [liveVendas, setLiveVendas] = useState(vendas);
+  useEffect(() => {
+    const subscription = client
+      .listen('*[_type == "sale"]')
+      .subscribe((update) => {
+        console.log("update", update.mutations);
+        update.mutations.forEach((mutation: any) => {
+          if (mutation.create) {
+            // Add the new post to livePosts
+            setLiveVendas((prev: any) => [mutation.create, ...prev]);
+          } else if (mutation.delete) {
+            // Remove the deleted post from livePosts
+            setLiveVendas((prev: any) =>
+              prev.filter((post: any) => post._id !== mutation.delete.id),
+            );
+          }
+        });
+      });
 
-  
-  //useEffect(() => {
-    //const vendasFiltered = vendas.filter(venda => {
-     // return venda.shippingMethod === "shipping" && /* Add condition to check if the item is shipped */;
-    //});
-   // setFilteredVendas(vendasFiltered);
- //}, [vendas]);
- 
+    // Cleanup subscription on unmount
+    return () => subscription.unsubscribe();
+  }, [password]);
 
-
-   // Check if the user is already logged in
-   useEffect(() => {
-    console.log("nookies")
+  // Check if the user is already logged in
+  useEffect(() => {
+    console.log("nookies");
     const cookies = parseCookies();
-    console.log(cookies)
-    const loggedIn = cookies['hasAccess'];
-    console.log(loggedIn)
+    console.log(cookies);
+    const loggedIn = cookies["hasAccess"];
+    console.log(loggedIn);
     if (loggedIn) {
       setPassword(true);
     }
@@ -42,8 +51,6 @@ const PainelDasVendas = ({vendas}: Props) => {
     const enteredPassword = (
       event.target as HTMLFormElement
     ).querySelector<HTMLInputElement>('input[name="password"]')?.value;
-
-    
 
     // Send a POST request to the API route with the entered password
     const response = await fetch("/api/pass", {
@@ -71,31 +78,29 @@ const PainelDasVendas = ({vendas}: Props) => {
     <div className="w-full h-screen">
       {!password ? (
         <>
-         <div className="w-full h-screen flex flex-col justify-center items-center">
+          <div className="w-full h-screen flex flex-col justify-center items-center">
+            <div className=" text-3xl mb-8">Entre Palavra-Passe</div>
 
-      
-          <div className=" text-3xl mb-8">Entre Palavra-Passe</div>
-
-          <form
-            className="w-full flex justify-center"
-            onSubmit={handlePasswordSubmit}
-          >
-            <input
-              className=" h-10  pl-6 bg-white text-black rounded-l-xl transition-all duration-150 w-[210px] sm:w-[300px] border-b-5 focus:rounded-l-3xl focus:outline-none"
-              type="password"
-              name="password"
-            />
-            <button className="bg-orange-300 rounded-r-xl w-32" type="submit">
-              Submit
-            </button>
-          </form>
+            <form
+              className="w-full flex justify-center"
+              onSubmit={handlePasswordSubmit}
+            >
+              <input
+                className=" h-10  pl-6 bg-white text-black rounded-l-xl transition-all duration-150 w-[210px] sm:w-[300px] border-b-5 focus:rounded-l-3xl focus:outline-none"
+                type="password"
+                name="password"
+              />
+              <button className="bg-orange-300 rounded-r-xl w-32" type="submit">
+                Submit
+              </button>
+            </form>
           </div>
         </>
       ) : (
         // Render the protected content
         <div className="w-full h-screen flex flex-col justify-start items-start">
           <h1 className="m-2 md:m-6 text-xl">Painel das Vendas</h1>
-          <Vendas vendas={vendas}/>
+          <Vendas vendas={liveVendas} />
         </div>
       )}
     </div>
